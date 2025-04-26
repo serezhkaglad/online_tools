77109c34ba9e5fc9fe48629b43c5a66a1863e39f
Сергей Кузьмин <pyromanh@gmail.com>
2025-04-26 11:42:34 +0300

    fixed firebase hosting issue

--- todo/todo/src/App.test.js
+++ todo/todo/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the todo app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/budget tracker/i);
   expect(linkElement).toBeInTheDocument();
 });
